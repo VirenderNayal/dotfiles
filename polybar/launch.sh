9e663f317543065639873479707b183b30f32417
Virender Nayal <virendernayal@protonmail.com>
2021-11-28 15:33:54 +0530

    added uncolored polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar barLeft 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar barMiddle 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar barRight 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
